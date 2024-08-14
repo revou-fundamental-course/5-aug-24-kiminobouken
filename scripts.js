2dab29480905bfa5a7225f18c1ba00314ac7f838
kiminobouken <jufrisaniardi@gmail.com>
2024-08-14 20:16:31 +0700

    third commit

--- scripts.js
+++ scripts.js
@@ -12,3 +12,29 @@ function hitungBMI() {
         alert("Silakan masukkan berat dan tinggi badan yang valid.");
     }
 }
+
+
+var calcdipslay = "
+"
+
+
+function cekStatusBMI(bmi) {
+    let status;
+
+    if (bmi < 18.5) {
+        status = "Status tubuh Anda adalah kekurangan berat badan.";
+    } else if (bmi >= 18.5 && bmi <= 24.9) {
+        status = "Status tubuh Anda adalah normal (ideal).";
+    } else if (bmi > 24.9 && bmi <= 29.9) {
+        status = "Status tubuh Anda adalah kelebihan berat badan.";
+    } else if (bmi >= 30) {
+        status = "Status tubuh Anda adalah kegemukan (obesitas).";
+    }
+
+    return status;
+}
+
+// Contoh penggunaan
+let bmi = 22.5; // Masukkan nilai BMI di sini
+console.log(cekStatusBMI(bmi));
+
